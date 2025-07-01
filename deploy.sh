2e18b6415c86061d62ca720fa6cf8e99f27cb84a
tnewbury <trevornewbury1@googlemail.com>
2025-07-01 13:54:14 +0100

    Merge pull request #7 from tnewbury/master
    Update to use API rather than Elastic Search

--- deploy.sh
+++ deploy.sh
@@ -23,10 +23,66 @@ function validate {
 
 }
 
+function sync_test {
+    
+    host="tubearchivist.local"
+    # make base folder
+    ssh "$host" "mkdir -p docker"
+
+    # copy project files to build image
+    rsync -a --progress --delete-after \
+        --exclude ".git" \
+        --exclude ".gitignore" \
+        --exclude "**/cache" \
+        --exclude "**/__pycache__/" \
+        . -e ssh "$host":tubearchivist-metrics
+
+    ssh "$host" "docker buildx build -t bbilly1/tubearchivist-metrics:latest tubearchivist-metrics --load"
+
+    ssh "$host" 'docker compose -f docker/docker-compose.yml up -d'
+
+}
+
+function sync_docker {
+
+    # check things
+    if [[ $(git branch --show-current) != 'master' ]]; then
+        echo 'you are not on master, dummy!'
+        return
+    fi
+
+    if [[ $(systemctl is-active docker) != 'active' ]]; then
+        echo "starting docker"
+        sudo systemctl start docker
+    fi
+
+    # checkout latest tag
+    git fetch --tags github master
+    VERSION=$(git tag | head -n 1)
+    git checkout "$VERSION"
+
+    echo "build and push tubearchivist-metrics:$VERSION ?"
+    read -rn 1
+
+    # start build
+    sudo docker buildx build \
+        --platform linux/amd64,linux/arm64 \
+        -t bbilly1/tubearchivist-metrics \
+        -t bbilly1/tubearchivist-metrics:"$VERSION" --push .
+
+    git checkout master
+
+}
+
+
 if [[ $1 == "validate" ]]; then
-    validate
+    validate "$2"
+elif [[ $1 == "test" ]]; then
+    sync_test
+elif [[ $1 == "docker" ]]; then
+    sync_docker
 else
-    echo "valid options are: validate"
+    echo "valid options are: validate | test | docker"
 fi
 
 exit 0
--- tubearchivist-metrics/environment.py
+++ tubearchivist-metrics/environment.py
@@ -15,16 +15,16 @@ class AppConfig:
         Reads in environment variables for the application to use.
         """
 
-        es_pass = os.environ.get("ES_PASSWORD")
-        es_user = os.environ.get("ES_USER", default="elastic")
-        es_url = os.environ.get("ES_URL", default="http://archivist-es:9200")
+
+        ta_key = os.environ.get("TA_KEY")
+        ta_url = os.environ.get("TA_URL")
         listen_port = os.environ.get("LISTEN_PORT", default="9934")
-        poll_interval = os.environ.get("POLL_INTERVAL", default="60")
+        poll_interval = os.environ.get("POLL_INTERVAL", default="120")
 
         application = {
-            "es_url": es_url,
-            "es_user": es_user,
-            "es_pass": es_pass,
+            "ta_key": ta_key,
+            "ta_url": ta_url,
+ 
             "listen_port": listen_port,
             "poll_interval": poll_interval,
         }
